48b05a327a5adede12543678e9de2e2268363c68
Ralph Plawetzki <ralph@purejava.org>
2024-01-04 18:06:29 +0100

    Add JUnit 5 dummy test and simplelogger.properties

---
+++ src/test/java/org/purejava/integrations/keychain/BitwardenAccessTest.java
@@ -0,0 +1,17 @@
+package org.purejava.integrations.keychain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BitwardenAccessTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
