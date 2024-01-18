041f7d188de663b81402d8c7e79a7db44693c2e0
Ralph Plawetzki <ralph@purejava.org>
2024-01-18 19:27:57 +0100

    Fix value for --module-version option

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.purejava.integrations.keychain.bitwarden {
-    requires java.desktop;
-    requires org.slf4j;
-    requires org.cryptomator.integrations.api;
-    requires sdk;
-
-    exports org.purejava.integrations.keychain;
-}
